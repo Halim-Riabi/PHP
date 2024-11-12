8f2e715a545fa9af0ff37e1791926fb3bbb3549b
guyroW <halimriabi001@gmail.com>
2024-11-12 09:38:42 +0100

    first commit

---
+++ prog.php
@@ -0,0 +1,24 @@
+
+<?php
+echo "My first PHP script!\n";
+
+$myname = "Halim";
+$mylastname ="Riabi";
+$myage= "23";
+
+echo "I'm $myname Riabi\n";
+
+echo "$mylastname $myname\n";
+print "$mylastname $myname\n";
+// print "first string" "second string";
+echo "first string ","second string\n";
+// print "first string ","second string"; false cuz print only output one single string
+
+$variable = print("hello\n");
+echo "$variable\n";
+$_newone = "78\n";
+echo $_newone;
+
+$_third3333=3;
+echo"  $_third3333\n";
+?>
