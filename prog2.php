b52c8726f74fc1f3cfcd84078d7239e23e10dce7
guyroW <halimriabi001@gmail.com>
2024-11-12 18:19:57 +0100

    constant tests

---
+++ prog2.php
@@ -0,0 +1,10 @@
+<?php
+define("first_constant", "C");
+define("second_one", 222);
+echo"this a constant " , first_constant , " and here's the second one " , second_one ;
+echo"\nfirst_constant\n";
+
+// define
+// define(string $constant_name, mixed $value [, bool $case_insensitive]): bool
+
+?>
