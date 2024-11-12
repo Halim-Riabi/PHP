fcf2b04b4c28849b92752e2dd1dcc9a983ee652b
guyroW <halimriabi001@gmail.com>
2024-11-12 10:05:18 +0100

    null tests

---
+++ prog1.php
@@ -0,0 +1,14 @@
+<?php
+
+$variable_test="haha";
+
+if(isset($variable_test)){
+echo"not null\n","$variable_test\n";
+// echo $variable_test;
+echo "it is not 'variable_test' null at all\n";
+}
+else{
+echo"null";
+}
+
+?>
